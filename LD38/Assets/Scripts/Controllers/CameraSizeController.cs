55fa9e7edb3c808582fcdbf94cef28f3a9445e42
Goto01 <kirill.tkachev.ktkach@gmail.com>
2017-04-22 00:41:11 +0300

    + Camera size controller

---
+++ LD38/Assets/Scripts/Controllers/CameraSizeController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Controllers
+{
+    class CameraSizeController : BaseController<CameraSizeController>
+    {
+        private Camera _camera;
+        [SerializeField] private int _cameraScale = 6;
+        
+        public int CameraScale { get { return _cameraScale; } }
+
+        private Camera Camera
+        {
+            get
+            {
+                if (_camera == null) _camera = Camera.main;
+                return _camera;
+            }
+        }
+        
+        protected virtual void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Keypad2)) _cameraScale--;
+            else if (Input.GetKeyDown(KeyCode.Keypad8)) _cameraScale++;
+        }
+
+        public void IncCameraScale()
+        {
+            ++_cameraScale;
+        }
+
+        public void DecCameraScale()
+        {
+            --_cameraScale;
+        }
+
+        protected virtual void LateUpdate()
+        {
+            SetCameraSize();
+        }
+        
+        private void SetCameraSize()
+        {
+            Camera.orthographicSize = (float)Screen.height / _cameraScale * .03125f;
+        }
+
+        public override void AwakeSingleton()
+        {
+            
+        }
+    }
+}
